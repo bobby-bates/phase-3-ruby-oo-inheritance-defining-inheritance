059b057cf049d1595e01f332f2efa082b14c65e5
Bobby Bates <robertfbatesjr@gmail.com>
2022-07-26 11:32:36 -0400

    completed assignment

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,9 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+
+class Car < Vehicle
+  # Overwrites the inherited #go method with one specific to Car
+  def go
+    'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!'
+  end
+end
